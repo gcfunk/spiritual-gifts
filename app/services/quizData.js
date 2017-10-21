6e51a55d84636f2678d1950a69fb55c17a255dee
Greg Funk <=>
2017-10-21 15:29:16 -0400

    Adds gifts to data

--- app/services/quizData.js
+++ app/services/quizData.js
@@ -5,14 +5,108 @@ angular.module('myApp').
 factory('quizData', function(){
   var quizData = {};
 
-  quizData.questions = [
-    { question: 'I work well under pressure and get to the heart of a matter and take decisive action' },
-    { question: 'Other people seem to have confidence in my ability to tell when something or someone is right or wrong' },
-    { question: 'I would rather deal with someone having problems personally instead of sending them to someone else' },
-    { question: 'I receive great satisfaction in sharing Christ freely and effectively with unbelieving persons' },
-    { question: 'I have known God\'s will with certainty in a specific situation even when concrete evidence was missing' }
-  ];
   quizData.currentQuestion = 0;
+
+  quizData.questions = [
+    {
+      question: 'I work well under pressure and get to the heart of a matter and take decisive action',
+      giftId: 0
+    },
+    {
+      question: 'Other people seem to have confidence in my ability to tell when something or someone is right or wrong',
+      giftId: 0
+    },
+    {
+      question: 'I would rather deal with someone having problems personally instead of sending them to someone else',
+      giftId: 0
+    },
+    {
+      question: 'I receive great satisfaction in sharing Christ freely and effectively with unbelieving persons',
+      giftId: 0
+    },
+    {
+      question: 'I have known God\'s will with certainty in a specific situation even when concrete evidence was missing',
+      giftId: 0
+    }
+  ];
+
+  quizData.gifts = [
+    {
+      id: 1,
+      name: 'Administration',
+      description: ''
+    },
+    {
+      id: 2,
+      name: 'Discernment',
+      description: ''
+    },
+    {
+      id: 3,
+      name: 'Encouragement',
+      description: ''
+    },
+    {
+      id: 4,
+      name: 'Evangelism',
+      description: ''
+    },
+    {
+      id: 5,
+      name: 'Faith',
+      description: ''
+    },
+    {
+      id: 6,
+      name: 'Giving',
+      description: ''
+    },
+    {
+      id: 7,
+      name: 'Helping',
+      description: ''
+    },
+    {
+      id: 8,
+      name: 'Knowledge',
+      description: ''
+    },
+    {
+      id: 9,
+      name: 'Leadership',
+      description: ''
+    },
+    {
+      id: 10,
+      name: 'Mercy',
+      description: ''
+    },
+    {
+      id: 11,
+      name: 'Prophecy',
+      description: ''
+    },
+    {
+      id: 12,
+      name: 'Service',
+      description: ''
+    },
+    {
+      id: 13,
+      name: 'Shepherding',
+      description: ''
+    },
+    {
+      id: 14,
+      name: 'Teaching',
+      description: ''
+    },
+    {
+      id: 15,
+      name: 'Wisdom',
+      description: ''
+    }
+  ];
 
   return quizData;
 });
