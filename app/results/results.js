ce0b1c80a593e0bd1dc58c11a96ce21092bb5904
Greg Funk <=>
2017-10-21 19:36:33 -0400

    Tally quiz score and show results

--- app/results/results.js
+++ app/results/results.js
@@ -12,4 +12,18 @@ angular.module('myApp.results', ['ngRoute'])
 .controller('ResultsCtrl', ['quizData', function(quizData) {
   var self = this;
   self.quizData = quizData;
+
+  // loop through each gift
+  angular.forEach(self.quizData.gifts, function (gift) {
+    // find all questions attached to the gift
+    var questions = self.quizData.questions.filter(function(question) {
+      return question.giftId === gift.id;
+    });
+
+    // the score is the sum of the user's answers
+    gift.score = 0;
+    angular.forEach(questions, function(question) {
+      gift.score += question.userAnswer ? question.userAnswer : 0;
+    });
+  });
 }]);
